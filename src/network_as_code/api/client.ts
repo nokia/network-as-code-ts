5d1a4c39428cff1828772d01839704c038d12dfe
hbirru <henok.birru@nokia.com>
2023-12-07 11:30:06 +0200

    QodAPI is added

--- src/network_as_code/api/client.ts
+++ src/network_as_code/api/client.ts
@@ -1,11 +1,28 @@
+import { QodAPI } from './qodApi';
 
-const QOS_BASE_URL_PROD = "https://quality-of-service-on-demand.p-eu.rapidapi.com"
-const QOS_BASE_URL_DEV = "https://qos-on-demand2.p-eu.rapidapi.com"
+const QOS_BASE_URL_PROD =
+	'https://quality-of-service-on-demand.p-eu.rapidapi.com';
+const QOS_BASE_URL_DEV = 'https://qos-on-demand2.p-eu.rapidapi.com';
 
 export class APIClient {
-	constructor(token: string, qos_base_url: string = QOS_BASE_URL_PROD, dev_mode: boolean = false, ...args: any[]) {
-        if(dev_mode && qos_base_url == QOS_BASE_URL_PROD){
-            qos_base_url = QOS_BASE_URL_DEV
-        }
-    }
+	sessions: QodAPI;
+	constructor(
+		token: string,
+		qosBaseUrl: string = QOS_BASE_URL_PROD,
+		devMode: boolean = false,
+		...args: any[]
+	) {
+		if (devMode && qosBaseUrl == QOS_BASE_URL_PROD) {
+			qosBaseUrl = QOS_BASE_URL_DEV;
+		}
+		this.sessions = new QodAPI(
+			qosBaseUrl,
+			token,
+			devMode
+				? qosBaseUrl
+						.replace('https://', '')
+						.replace('p-eu', 'nokia-dev')
+				: qosBaseUrl.replace('https://', '').replace('p-eu', 'nokia')
+		);
+	}
 }
---
+++ src/network_as_code/api/qodApi.ts
@@ -0,0 +1,135 @@
+import axios, { AxiosInstance } from 'axios';
+import { Device, DeviceIpv4Addr } from '../models/device';
+import { QoDSession } from '../models/session';
+/**
+ *  Qod API, that sends requests to the API via httpx calls
+ */
+export class QodAPI {
+	/**
+ *  Methods takes rapidHost, rapidKey, and baseUrl.
+ * Args:
+            rapidHost (str): RapidAPI Host
+            rapidKey (str): RapidAPI Key
+            baseUrl (str): URL for
+    */
+	private client: AxiosInstance;
+	constructor(baseURL: string, rapidKey: string, rapidHost: string) {
+		this.client = axios.create({
+			baseURL,
+			headers: {
+				'content-type': 'application/json',
+				'X-RapidAPI-Key': rapidKey,
+				'X-RapidAPI-Host': rapidHost,
+			},
+		});
+	}
+
+	/**
+ *  Function that hits the create session endpoint with the data
+ * 
+ * #### Args:
+            profile (any): Name of the requested QoS profile.
+            serviceIpv4 (any): IPv4 address of the service.
+            serviceIpv6 (optional): IPv6 address of the service.
+            devicePorts (optional): List of the device ports.
+            servicePorts (optional): List of the application server ports.
+            duration (optional): Session duration in seconds.
+            notificationUrl (optional): Notification URL for session-related events.
+            notificationToken (optional): Security bearer token to authenticate registration of session.
+
+        Returns:
+            Session: response of the endpoint, ideally a Session
+ */
+	async createSession(
+		sid: any,
+		profile: any,
+		serviceIpv6: any,
+		serviceIpv4?: string,
+		phoneNumber?: string,
+		ipv4Address?: string | DeviceIpv4Addr,
+		devicePorts: any | undefined = undefined,
+		servicePorts: any | undefined = undefined,
+		duration = undefined,
+		notificationUrl = undefined,
+		notificationAuthToken = undefined
+	) {
+		let sessionPayload: any = {
+			qosProfile: profile,
+			device: { ipv4Address: {} },
+			applicationServer: { ipv4Address: serviceIpv4 },
+			devicePorts: devicePorts ? devicePorts : undefined,
+			applicationServerPorts: servicePorts ? servicePorts : undefined,
+		};
+
+		if (sid) {
+			sessionPayload['device']['networkAccessIdentifier'] = sid;
+		}
+
+		if (ipv4Address) {
+			if ((ipv4Address as DeviceIpv4Addr).publicAddress) {
+				sessionPayload['device']['ipv4Address']['publicAddress'] = (
+					ipv4Address as DeviceIpv4Addr
+				).publicAddress;
+			}
+			if ((ipv4Address as DeviceIpv4Addr).privateAddress) {
+				sessionPayload['device']['ipv4Address']['privateAddress'] = (
+					ipv4Address as DeviceIpv4Addr
+				).privateAddress;
+			}
+			if ((ipv4Address as DeviceIpv4Addr).publicPort) {
+				sessionPayload['device']['ipv4Address']['publicPort'] = (
+					ipv4Address as DeviceIpv4Addr
+				).publicPort;
+			}
+		}
+
+		if (phoneNumber) {
+			sessionPayload['device']['phoneNumber'] = phoneNumber;
+		}
+
+		if (serviceIpv6) {
+			sessionPayload['applicationServer']['ipv6Address'] = serviceIpv6;
+		}
+
+		if (duration) {
+			sessionPayload['duration'] = duration;
+		}
+
+		if (notificationUrl) {
+			sessionPayload['notificationUrl'] = notificationUrl;
+		}
+
+		if (notificationAuthToken) {
+			sessionPayload['notificationAuthToken'] =
+				'Bearer ' + notificationAuthToken;
+		}
+
+		let response = await this.client.post('/sessions', sessionPayload);
+		// ADD ERROR HANDLER
+
+		return response;
+	}
+
+		/**
+ *  This function retrieves all sessions given a device_id
+ * Args:
+            deviceId (dict): The dict with device-id of the device whose sessions to retrieve
+
+        Returns:
+            list: returns list of session
+ */
+	async getAllSessions(device: Device) {
+		let url = "";
+
+		if(device.networkAccessIdentifier){
+			url = `/sessions?networkAccessIdentifier=${device.networkAccessIdentifier}`
+		}else if(device.phoneNumber) {
+			url = `/sessions?phoneNumber=${device.phoneNumber}`
+		}
+
+		let response = this.client.get(url);
+
+		// ERROR HANDLER
+		return response;
+	}
+}
