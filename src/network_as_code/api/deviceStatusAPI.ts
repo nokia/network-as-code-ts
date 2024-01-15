aca85d144f2e26c53060a30c52501126c0862fcc
hbirru <henok.birru@nokia.com>
2024-01-15 10:50:50 +0200

    Error handler method added to deviceStatusAPI

--- src/network_as_code/api/deviceStatusAPI.ts
+++ src/network_as_code/api/deviceStatusAPI.ts
@@ -1,12 +1,12 @@
+import { errorHandler } from "../errors";
 import { Device } from "../models/device";
 import { Subscription } from "../models/deviceStatus";
 import { SubscribeOptionalArgs } from "../namespaces/deviceStatus";
 
-
 export class DeviceStatusAPI {
     private baseUrl: string;
     private headers: HeadersInit;
-    
+
     constructor(base_url: string, rapid_key: string, rapid_host: string) {
         this.baseUrl = base_url;
         this.headers = {
@@ -15,17 +15,22 @@ export class DeviceStatusAPI {
             "X-RapidAPI-Key": rapid_key,
         };
     }
-    
-    async subscribe(device: Device, eventType: string, notificationUrl: string, optionalArgs?: SubscribeOptionalArgs): Promise<Subscription> {
+
+    async subscribe(
+        device: Device,
+        eventType: string,
+        notificationUrl: string,
+        optionalArgs?: SubscribeOptionalArgs
+    ): Promise<Subscription> {
         const body: any = {
             subscriptionDetail: {
                 device: device.toJson(),
-                eventType: eventType
+                eventType: eventType,
             },
             webhook: {
-                notificationUrl: notificationUrl
-            }
-        }
+                notificationUrl: notificationUrl,
+            },
+        };
 
         if (optionalArgs) {
             if (optionalArgs.maxNumberOfReports) {
@@ -33,35 +38,49 @@ export class DeviceStatusAPI {
             }
 
             if (optionalArgs.subscriptionExpireTime) {
-                body.subscriptionExpireTime = optionalArgs.subscriptionExpireTime;
+                body.subscriptionExpireTime =
+                    optionalArgs.subscriptionExpireTime;
             }
 
             if (optionalArgs.notificationAuthToken) {
-                body.webhook.notificationAuthToken = optionalArgs.notificationAuthToken;
+                body.webhook.notificationAuthToken =
+                    optionalArgs.notificationAuthToken;
             }
         }
 
         const response = await fetch(`${this.baseUrl}/event-subscriptions`, {
             method: "POST",
             headers: this.headers,
-            body: JSON.stringify(body)
+            body: JSON.stringify(body),
         });
+
+        errorHandler(response);
 
         return response.json();
     }
 
     async delete(eventSubscriptionId: string) {
-        const response = await fetch(`${this.baseUrl}/event-subscriptions/${eventSubscriptionId}`, {
-            method: "DELETE",
-            headers: this.headers
-        })
+        const response = await fetch(
+            `${this.baseUrl}/event-subscriptions/${eventSubscriptionId}`,
+            {
+                method: "DELETE",
+                headers: this.headers,
+            }
+        );
+
+        errorHandler(response);
     }
 
     async get(eventSubscriptionId: string) {
-        const response = await fetch(`${this.baseUrl}/event-subscriptions/${eventSubscriptionId}`, {
-            method: "GET",
-            headers: this.headers
-        })
+        const response = await fetch(
+            `${this.baseUrl}/event-subscriptions/${eventSubscriptionId}`,
+            {
+                method: "GET",
+                headers: this.headers,
+            }
+        );
+
+        errorHandler(response);
 
         return response.json();
     }
