f24ede09566ffb10fbee979e7d6785bf952645e0
hbirru <henok.birru@nokia.com>
2024-06-12 19:00:28 +0300

    retry-async added to fix integration test

--- integration-tests/qos.itest.ts
+++ integration-tests/qos.itest.ts
@@ -2,7 +2,8 @@ import { beforeAll, beforeEach, describe, expect, test } from "@jest/globals";
 import "dotenv/config";
 import { NetworkAsCodeClient } from "../src";
 import { QoDSession } from "../src/models/session";
-import { Device, DeviceIpv4Addr } from '../src/models/device';
+import { Device, DeviceIpv4Addr } from "../src/models/device";
+import retry from "async-retry";
 
 let client: NetworkAsCodeClient;
 
@@ -19,28 +20,56 @@ describe("Qos", () => {
     let device: Device;
     let session: QoDSession;
     beforeEach(async () => {
-        const random = Math.floor(Math.random() * 1000) + 1;
-        device = client.devices.get(
-            `test-device${random}@testcsp.net`,
-            {
-                publicAddress: "1.1.1.2",
-                privateAddress: "1.1.1.2",
-                publicPort: 80,
-            },
-            undefined,
-            "9382948473"
-        );
-
-        session = await device.createQodSession(
-            "QOS_L",
-            "5.6.7.8",
-            "2041:0000:140F::875B:131B"
-        );
+        try {
+            const random = Math.floor(Math.random() * 1000) + 1;
+            device = client.devices.get(
+                `test-device${random}@testcsp.net`,
+                {
+                    publicAddress: "1.1.1.2",
+                    privateAddress: "1.1.1.2",
+                    publicPort: 80,
+                },
+                undefined,
+                "9382948473"
+            );
+            const createQodSessionWithRetry = async () => {
+                return retry(
+                    async (bail) => {
+                        try {
+                            const session = await device.createQodSession(
+                                "QOS_L",
+                                "5.6.7.8",
+                                "2041:0000:140F::875B:131B"
+                            );
+                            return session;
+                        } catch (error: any) {
+                            if (error.response && error.response.status < 500) {
+                                bail(error);
+                            } else {
+                                throw error;
+                            }
+                        }
+                    },
+                    {
+                        retries: 5,
+                    }
+                );
+            };
+            session = (await createQodSessionWithRetry()) as QoDSession;
+        } catch (error) {
+            console.error("Error during setup:", error);
+            throw error;
+        }
     });
 
     afterEach(async () => {
-        await session.deleteSession();
+        try {
+            await session.deleteSession();
+        } catch (error) {
+            console.error("Error during teardown:", error);
+        }
     });
+
     test("should get a device", () => {
         expect((device.ipv4Address as DeviceIpv4Addr).publicAddress).toEqual(
             "1.1.1.2"
