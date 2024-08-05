dbe05a37a6b524ce28cea70c57bb4ff937673a0e
Sami Lahtinen <sami.lahtinen@nokia.com>
2024-08-05 14:16:22 +0300

    We don't have to live in ES2016 world, we can have top-level async

--- examples/qod-session.ts
+++ examples/qod-session.ts
@@ -4,65 +4,60 @@
 
 import { NetworkAsCodeClient } from 'network-as-code';
 
-import { Device, DeviceIpv4Addr } from "network-as-code/models/device";
+// Begin by creating a client for Network as Code:
+const client = new NetworkAsCodeClient("<your-application-key-here>");
 
-// We are executing inside an async function to have access to await
-const main = async () => {
-    // Begin by creating a client for Network as Code:
-    const client = new NetworkAsCodeClient("<your-application-key-here>");
+// The "device@testcsp.net" should be replaced
+// with a test device copied from your Developer Sandbox
+// Or you can identify a device with its ID,
+// IP address(es) and optionally, a phone number
+const myDevice = client.devices.get({
+    networkAccessIdentifier: "device@testcsp.net",
+    ipv4Address: {
+        publicAddress: "233.252.0.2",
+        privateAddress: "192.0.2.25",
+        publicPort: 80,
+    },
+    Ipv6Address: "2041:0000:140F::875B:131B",
+    phoneNumber: "36721601234567"
+});
 
-    // The "device@testcsp.net" should be replaced
-    // with a test device copied from your Developer Sandbox
-    // Or you can identify a device with its ID,
-    // IP address(es) and optionally, a phone number
-    const myDevice = client.devices.get({
-        networkAccessIdentifier: "device@testcsp.net",
-        ipv4Address: {
-            publicAddress: "233.252.0.2",
-            privateAddress: "192.0.2.25",
-            publicPort: 80,
-        },
-        Ipv6Address: "2041:0000:140F::875B:131B",
-        phoneNumber: "36721601234567"
-    });
+// Create a QoD session with QOS_L (large bandwidth)
+// that lasts for 3,600 seconds (1 hour).
+// For TypeScript, values can be expressed directly,
+// but the QoS profile comes before the IP address(es) and duration:
+const mySession = await myDevice.createQodSession("QOS_L",{
+    serviceIpv4: "233.252.0.2",
+    serviceIpv6: "2001:db8:1234:5678:9abc:def0:fedc:ba98",
+    duration: 3600,
+    // Use HTTPS to send or subscribe to notifications
+    notificationUrl: "https://notify.me/here",
+    notificationAuthToken: "replace-with-your-auth-token"
+});
 
-    // Create a QoD session with QOS_L (large bandwidth)
-    // that lasts for 3,600 seconds (1 hour).
-    // For TypeScript, values can be expressed directly,
-    // but the QoS profile comes before the IP address(es) and duration:
-    const mySession = await myDevice.createQodSession("QOS_L",{
-        serviceIpv4: "233.252.0.2",
-        serviceIpv6: "2001:db8:1234:5678:9abc:def0:fedc:ba98",
-        duration: 3600,
-        // Use HTTPS to send or subscribe to notifications
-        notificationUrl: "https://notify.me/here",
-        notificationAuthToken: "replace-with-your-auth-token"
-    });
+// Let's confirm that the device has the newly created session
+console.log(myDevice.sessions());
 
-    // Let's confirm that the device has the newly created session
-    console.log(myDevice.sessions());
+// This is how you get all sessions for a device
+const allSessions = await myDevice.sessions();
 
-    // This is how you get all sessions for a device
-    const allSessions = await myDevice.sessions();
+// And you can also get a specific session.
+// Notice how we can use the number '0'
+// to retrieve our first session in this case:
+const firstSession = await allSessions[0];
 
-    // And you can also get a specific session.
-    // Notice how we can use the number '0'
-    // to retrieve our first session in this case:
-    const firstSession = await allSessions[0];
+// Show a list of all the QoD sessions associated with a device
+console.log(myDevice.sessions());
 
-    // Show a list of all the QoD sessions associated with a device
-    console.log(myDevice.sessions());
+// You can also show the duration of a given session
+console.log(mySession.duration());
 
-    // You can also show the duration of a given session
-    console.log(mySession.duration());
+// Or use these to check when your session started/expires:
+console.log(mySession.startedAt);
+console.log(mySession.expiresAt);
 
-    // Or use these to check when your session started/expires:
-    console.log(mySession.startedAt);
-    console.log(mySession.expiresAt);
+// Additionally, you can delete a specific session like so:
+mySession.deleteSession();
 
-    // Additionally, you can delete a specific session like so:
-    mySession.deleteSession();
-
-    // Finally, clear out all sessions for a device when you're done:
-    myDevice.clearSessions()
-};
+// Finally, clear out all sessions for a device when you're done:
+myDevice.clearSessions()
--- examples/slice-creation.ts
+++ examples/slice-creation.ts
@@ -4,92 +4,79 @@
 
 import { NetworkAsCodeClient } from "network-as-code";
 
-import { Device, DeviceIpv4Addr } from "network-as-code/models/device";
+const client = new NetworkAsCodeClient("<your-application-key-here>");
 
-import {
-    AreaOfService,
-    NetworkIdentifier,
-    Point,
-    Slice,
-    SliceInfo,
-    Throughput
-} from "network-as-code/models/slice";
-
-const main = async () => {
-    const client = new NetworkAsCodeClient("<your-application-key-here>");
-
-    // Creation of a slice:
-    // We use the country code (MCC) and network code (MNC) to identify the network
-    // Different types of slices can be requested using service type and differentiator
-    // Area of the slice must also be described in geo-coordinates
-    const mySlice = await client.slices.create(
-        { mcc: "236", mnc: "30" },
-        { serviceType: "eMBB", differentiator: "123456" },
-        "https://notify.me/here",
-        {
-            name: "slice-name",
-            notificationAuthToken: "replace-with-your-auth-token",
-            areaOfService: {
-                polygon: [
-                    {
-                        latitude: 23.0,
-                        longitude: 23.0
-                    },
-                    {
-                        latitude: 23.0,
-                        longitude: 23.0
-                    },
-                ],
-            },
-            maxDataConnections: 1000,
-            maxDevices: 5,
-            sliceDownlinkThroughput: {
-                guaranteed: 3415,
-                maximum: 1234324
-            },
-            sliceUplinkThroughput: {
-                guaranteed: 3415,
-                maximum: 1234324
-            },
-            deviceDownlinkThroughput: {
-                guaranteed: 3415,
-                maximum: 1234324
-            },
-            deviceUplinkThroughput: {
-                guaranteed: 3415,
-                maximum: 1234324
-            }
+// Creation of a slice:
+// We use the country code (MCC) and network code (MNC) to identify the network
+// Different types of slices can be requested using service type and differentiator
+// Area of the slice must also be described in geo-coordinates
+const mySlice = await client.slices.create(
+    { mcc: "236", mnc: "30" },
+    { serviceType: "eMBB", differentiator: "123456" },
+    "https://notify.me/here",
+    {
+        name: "slice-name",
+        notificationAuthToken: "replace-with-your-auth-token",
+        areaOfService: {
+            polygon: [
+                {
+                    latitude: 23.0,
+                    longitude: 23.0
+                },
+                {
+                    latitude: 23.0,
+                    longitude: 23.0
+                },
+            ],
+        },
+        maxDataConnections: 1000,
+        maxDevices: 5,
+        sliceDownlinkThroughput: {
+            guaranteed: 3415,
+            maximum: 1234324
+        },
+        sliceUplinkThroughput: {
+            guaranteed: 3415,
+            maximum: 1234324
+        },
+        deviceDownlinkThroughput: {
+            guaranteed: 3415,
+            maximum: 1234324
+        },
+        deviceUplinkThroughput: {
+            guaranteed: 3415,
+            maximum: 1234324
         }
-    );
+    }
+);
 
-    // Get a slice by using an index
-    // or remove the index '[0]' to get all slices
-    const sliceByIndex = await client.slices.getAll()[0];
+// Get a slice by using an index
+// or remove the index '[0]' to get all slices
+const sliceByIndex = await client.slices.getAll()[0];
 
-    // Get a slice using the ID parameter
-    const sliceByName = await client.slices.get(mySlice.name);
+// Get a slice using the ID parameter
+const sliceByName = await client.slices.get(mySlice.name);
 
-    // Modify a slice:
-    // Remember to reconfigure all the parameters you wish to modify,
-    // The modify() method does not preserve previously configure ones.
-    mySlice.modify(
-        {
-            sliceDownlinkThroughput: { guaranteed: 10, maximum: 10 },
-            sliceUplinkThroughput: { guaranteed: 10, maximum: 10 },
-            deviceDownlinkThroughput: { guaranteed: 10, maximum: 10 },
-            deviceUplinkThroughput: { guaranteed: 10, maximum: 10 },
-            maxDataConnections: 12,
-            maxDevices: 3
-        }
-    );
+// Modify a slice:
+// Remember to reconfigure all the parameters you wish to modify,
+// The modify() method does not preserve previously configure ones.
+mySlice.modify(
+    {
+        sliceDownlinkThroughput: { guaranteed: 10, maximum: 10 },
+        sliceUplinkThroughput: { guaranteed: 10, maximum: 10 },
+        deviceDownlinkThroughput: { guaranteed: 10, maximum: 10 },
+        deviceUplinkThroughput: { guaranteed: 10, maximum: 10 },
+        maxDataConnections: 12,
+        maxDevices: 3
+    }
+);
 
-    // Then, activate a slice
-    mySlice.activate();
+// Then, activate a slice
+mySlice.activate();
 
-    // Or deactivate it
-    mySlice.deactivate();
+// Or deactivate it
+mySlice.deactivate();
 
-    // Finally, you can also delete it like so:
-    // Remember to deactivate it first.
-    await mySlice.delete();
-};
+// Finally, you can also delete it like so:
+// Remember to deactivate it first.
+await mySlice.delete();
