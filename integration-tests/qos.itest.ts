a344dc804baa545f14d0589b5dbcc867f6a369b7
hbirru <henok.birru@nokia.com>
2024-09-23 14:06:22 +0300

    integration tests are modified for the additional QoD attributes

--- integration-tests/qos.itest.ts
+++ integration-tests/qos.itest.ts
@@ -61,190 +61,210 @@ describe("Qos", () => {
         });
         expect(session.status).toEqual("REQUESTED");
         expect(session.profile).toEqual("QOS_L");
+        expect(session.serviceIpv4).toEqual("5.6.7.8");
+        expect(session.serviceIpv6).toEqual("2041:0000:140F::875B:131B");
         await session.deleteSession();
     });
 
-//     test("should create a session to a device with phone number", async () => {
-//         const session = await deviceWithPhoneNumber.createQodSession("QOS_L", {
-//             duration: 3600,
-//             serviceIpv4: "5.6.7.8",
-//             serviceIpv6: "2041:0000:140F::875B:131B",
-//         });
-//         expect(session.status).toEqual("REQUESTED");
-//         await session.deleteSession();
-//     });
+    test("should create a session to a device with phone number", async () => {
+        const session = await deviceWithPhoneNumber.createQodSession("QOS_L", {
+            duration: 3600,
+            serviceIpv4: "5.6.7.8",
+            serviceIpv6: "2041:0000:140F::875B:131B",
+        });
+        expect(session.status).toEqual("REQUESTED");
+        expect(session.device.phoneNumber).toEqual(
+            deviceWithPhoneNumber.phoneNumber
+        );
+        expect(session.serviceIpv4).toEqual("5.6.7.8");
+        expect(session.serviceIpv6).toEqual("2041:0000:140F::875B:131B");
+        await session.deleteSession();
+    });
 
-//     test("should create a session with medium profile", async () => {
-//         const session = await device.createQodSession("QOS_M", {
-//             duration: 3600,
-//             serviceIpv4: "5.6.7.8",
-//             serviceIpv6: "2041:0000:140F::875B:131B",
-//         });
-//         expect(session.status).toEqual("REQUESTED");
-//         expect(session.profile).toEqual("QOS_M");
-//         await session.deleteSession();
-//     });
+    test("should create a session with medium profile", async () => {
+        const session = await device.createQodSession("QOS_M", {
+            duration: 3600,
+            serviceIpv4: "5.6.7.8",
+            serviceIpv6: "2041:0000:140F::875B:131B",
+        });
+        expect(session.status).toEqual("REQUESTED");
+        expect(session.profile).toEqual("QOS_M");
+        await session.deleteSession();
+    });
 
-//     test("should create a session with small profile", async () => {
-//         const session = await device.createQodSession("QOS_S", {
-//             duration: 3600,
-//             serviceIpv4: "5.6.7.8",
-//             serviceIpv6: "2041:0000:140F::875B:131B",
-//         });
-//         expect(session.status).toEqual("REQUESTED");
-//         expect(session.profile).toEqual("QOS_S");
-//         await session.deleteSession();
-//     });
+    test("should create a session with small profile", async () => {
+        const session = await device.createQodSession("QOS_S", {
+            duration: 3600,
+            serviceIpv4: "5.6.7.8",
+            serviceIpv6: "2041:0000:140F::875B:131B",
+        });
+        expect(session.status).toEqual("REQUESTED");
+        expect(session.profile).toEqual("QOS_S");
+        await session.deleteSession();
+    });
 
-//     test("should create a session with low latency profile", async () => {
-//         const session = await device.createQodSession("QOS_E", {
-//             duration: 3600,
-//             serviceIpv4: "5.6.7.8",
-//             serviceIpv6: "2041:0000:140F::875B:131B",
-//         });
-//         expect(session.status).toEqual("REQUESTED");
-//         expect(session.profile).toEqual("QOS_E");
-//         await session.deleteSession();
-//     });
+    test("should create a session with low latency profile", async () => {
+        const session = await device.createQodSession("QOS_E", {
+            duration: 3600,
+            serviceIpv4: "5.6.7.8",
+            serviceIpv6: "2041:0000:140F::875B:131B",
+        });
+        expect(session.status).toEqual("REQUESTED");
+        expect(session.profile).toEqual("QOS_E");
+        await session.deleteSession();
+    });
 
-//     test("should get one session", async () => {
-//         const session = await device.createQodSession("QOS_E", {
-//             duration: 3600,
-//             serviceIpv4: "5.6.7.8",
-//             serviceIpv6: "2041:0000:140F::875B:131B",
-//         });
-//         const fetchedSession = await client.sessions.get(session.id);
-//         expect(session.id).toEqual(fetchedSession.id);
-//         await session.deleteSession();
+    test("should get one session", async () => {
+        const session = await device.createQodSession("QOS_E", {
+            duration: 3600,
+            serviceIpv4: "5.6.7.8",
+            serviceIpv6: "2041:0000:140F::875B:131B",
+        });
+        const fetchedSession = await client.sessions.get(session.id);
+        expect(session.id).toEqual(fetchedSession.id);
+        expect(fetchedSession.device.networkAccessIdentifier).toEqual(
+            device.networkAccessIdentifier
+        );
+        await session.deleteSession();
 
-//         try {
-//             await client.sessions.get(session.id);
-//             expect(true).toBe(false);
-//         } catch (error) {
-//             expect(true).toBe(true);
-//         }
-//     });
+        try {
+            await client.sessions.get(session.id);
+            expect(true).toBe(false);
+        } catch (error) {
+            expect(true).toBe(true);
+        }
+    });
 
-//     test("should get all sessions", async () => {
-//         await device.createQodSession("QOS_E", {
-//             duration: 3600,
-//             serviceIpv4: "5.6.7.8",
-//             serviceIpv6: "2041:0000:140F::875B:131B",
-//         });
-//         const sessions = await device.sessions();
+    test("should get all sessions", async () => {
+        await device.createQodSession("QOS_E", {
+            duration: 3600,
+            serviceIpv4: "5.6.7.8",
+            serviceIpv6: "2041:0000:140F::875B:131B",
+        });
+        const sessions = await device.sessions();
 
-//         expect(sessions.length).toBeGreaterThan(0);
+        expect(sessions.length).toBeGreaterThan(0);
 
-//         await device.clearSessions();
-//     });
+        await device.clearSessions();
+    });
 
-//     test("should create a session with service port", async () => {
-//         const session = await device.createQodSession("QOS_L", {
-//             duration: 3600,
-//             serviceIpv4: "5.6.7.8",
-//             serviceIpv6: "2041:0000:140F::875B:131B",
-//             servicePorts: { ports: [80] },
-//         });
-//         expect(session.status).toEqual("REQUESTED");
-//         expect(session.profile).toEqual("QOS_L");
-//         await session.deleteSession();
-//     });
+    test("should create a session with service port", async () => {
+        const session = await device.createQodSession("QOS_L", {
+            duration: 3600,
+            serviceIpv4: "5.6.7.8",
+            serviceIpv6: "2041:0000:140F::875B:131B",
+            servicePorts: { ports: [80] },
+        });
+        expect(session.status).toEqual("REQUESTED");
+        expect(session.profile).toEqual("QOS_L");
+        expect(session.servicePorts?.ports).toEqual([80]);
+        await session.deleteSession();
+    });
 
-//     test("should create a session with service port range", async () => {
-//         const session = await device.createQodSession("QOS_M", {
-//             duration: 3600,
-//             serviceIpv4: "5.6.7.8",
-//             serviceIpv6: "2041:0000:140F::875B:131B",
-//             servicePorts: { ranges: [{ from: 80, to: 443 }] },
-//         });
+    test("should create a session with service port range", async () => {
+        const session = await device.createQodSession("QOS_M", {
+            duration: 3600,
+            serviceIpv4: "5.6.7.8",
+            serviceIpv6: "2041:0000:140F::875B:131B",
+            servicePorts: { ranges: [{ from: 80, to: 443 }] },
+        });
 
-//         expect(session.status).toEqual("REQUESTED");
-//         expect(session.profile).toEqual("QOS_M");
-//         await session.deleteSession();
-//     });
+        expect(session.status).toEqual("REQUESTED");
+        expect(session.profile).toEqual("QOS_M");
+        expect(session.servicePorts?.ranges).toEqual([{ from: 80, to: 443 }]);
+        await session.deleteSession();
+    });
 
-//     test("should create a session with device port", async () => {
-//         const session = await device.createQodSession("QOS_M", {
-//             duration: 3600,
-//             serviceIpv4: "5.6.7.8",
-//             serviceIpv6: "2041:0000:140F::875B:131B",
-//             devicePorts: { ports: [80] },
-//         });
+    test("should create a session with device port", async () => {
+        const session = await device.createQodSession("QOS_M", {
+            duration: 3600,
+            serviceIpv4: "5.6.7.8",
+            serviceIpv6: "2041:0000:140F::875B:131B",
+            devicePorts: { ports: [80] },
+        });
 
-//         expect(session.status).toEqual("REQUESTED");
-//         expect(session.profile).toEqual("QOS_M");
-//         await session.deleteSession();
-//     });
+        expect(session.status).toEqual("REQUESTED");
+        expect(session.profile).toEqual("QOS_M");
+        await session.deleteSession();
+    });
 
-//     test("should create a session with device port range", async () => {
-//         const session = await device.createQodSession("QOS_M", {
-//             duration: 3600,
-//             serviceIpv4: "5.6.7.8",
-//             serviceIpv6: "2041:0000:140F::875B:131B",
-//             devicePorts: { ranges: [{ from: 80, to: 443 }] },
-//         });
+    test("should create a session with device port range", async () => {
+        const session = await device.createQodSession("QOS_M", {
+            duration: 3600,
+            serviceIpv4: "5.6.7.8",
+            serviceIpv6: "2041:0000:140F::875B:131B",
+            devicePorts: { ranges: [{ from: 80, to: 443 }] },
+        });
 
-//         expect(session.status).toEqual("REQUESTED");
-//         expect(session.profile).toEqual("QOS_M");
-//         await session.deleteSession();
-//     });
+        expect(session.status).toEqual("REQUESTED");
+        expect(session.profile).toEqual("QOS_M");
+        await session.deleteSession();
+    });
 
-//     test("should create a session with duration", async () => {
-//         const session = await device.createQodSession("QOS_L", {
-//             serviceIpv4: "5.6.7.8",
-//             serviceIpv6: "2041:0000:140F::875B:131B",
-//             duration: 60,
-//         });
+    test("should create a session with duration", async () => {
+        const session = await device.createQodSession("QOS_L", {
+            serviceIpv4: "5.6.7.8",
+            serviceIpv6: "2041:0000:140F::875B:131B",
+            duration: 60,
+        });
 
-//         expect(session.startedAt).toBeTruthy();
-//         expect(session.expiresAt).toBeTruthy();
-//         expect(session.duration()).toEqual(60);
-//         await session.deleteSession();
-//     });
+        expect(session.startedAt).toBeTruthy();
+        expect(session.expiresAt).toBeTruthy();
+        expect(session.duration()).toEqual(60);
+        await session.deleteSession();
+    });
 
-//     test("should create a session with notification url", async () => {
-//         const session = await device.createQodSession("QOS_L", {
-//             duration: 3600,
-//             serviceIpv4: "5.6.7.8",
-//             serviceIpv6: "2041:0000:140F::875B:131B",
-//             notificationAuthToken: "c8974e592c2fa383d4a3960714",
-//             notificationUrl: "https://example.com/notifications",
-//         });
+    test("should create a session with notification url", async () => {
+        const session = await device.createQodSession("QOS_L", {
+            duration: 3600,
+            serviceIpv4: "5.6.7.8",
+            serviceIpv6: "2041:0000:140F::875B:131B",
+            notificationAuthToken: "c8974e592c2fa383d4a3960714",
+            notificationUrl: "https://example.com/notifications",
+        });
 
-//         expect(session.status).toEqual("REQUESTED");
-//         expect(session.profile).toEqual("QOS_L");
-//         await session.deleteSession();
-//     });
+        expect(session.status).toEqual("REQUESTED");
+        expect(session.profile).toEqual("QOS_L");
+        await session.deleteSession();
+    });
 
-//     test("should create a session with public and private ipv4", async () => {
-//         device = client.devices.get({
-//             networkAccessIdentifier: "test-device@testcsp.net",
-//             ipv4Address: {
-//                 publicAddress: "1.1.1.2",
-//                 privateAddress: "1.1.1.2",
-//             },
-//         });
-//         const session = await device.createQodSession("QOS_L", {
-//             duration: 3600,
-//             serviceIpv4: "5.6.7.8",
-//         });
+    test("should create a session with public and private ipv4", async () => {
+        device = client.devices.get({
+            networkAccessIdentifier: "test-device@testcsp.net",
+            ipv4Address: {
+                publicAddress: "1.1.1.2",
+                privateAddress: "1.1.1.2",
+            },
+        });
+        const session = await device.createQodSession("QOS_L", {
+            duration: 3600,
+            serviceIpv4: "5.6.7.8",
+        });
 
-//         await session.deleteSession();
-//     });
+        expect(session.serviceIpv4).toEqual("5.6.7.8");
+        expect(session.device.ipv4Address?.publicAddress).toEqual("1.1.1.2");
+        expect(session.device.ipv4Address?.privateAddress).toEqual("1.1.1.2");
 
-//     test("should create a session with public ipv4 and public port", async () => {
-//         device = client.devices.get({
-//             networkAccessIdentifier: "test-device@testcsp.net",
-//             ipv4Address: {
-//                 publicAddress: "1.1.1.2",
-//                 publicPort: 80,
-//             },
-//         });
-//         const session = await device.createQodSession("QOS_L", {
-//             duration: 3600,
-//             serviceIpv4: "5.6.7.8",
-//         });
+        await session.deleteSession();
+    });
 
-//         await session.deleteSession();
-//     });
+    test("should create a session with public ipv4 and public port", async () => {
+        device = client.devices.get({
+            networkAccessIdentifier: "test-device@testcsp.net",
+            ipv4Address: {
+                publicAddress: "1.1.1.2",
+                publicPort: 80,
+            },
+        });
+        const session = await device.createQodSession("QOS_L", {
+            duration: 3600,
+            serviceIpv4: "5.6.7.8",
+        });
+
+        expect(session.serviceIpv4).toEqual("5.6.7.8");
+        expect(session.device.ipv4Address?.publicAddress).toEqual("1.1.1.2");
+        expect(session.device.ipv4Address?.publicPort).toEqual(80);
+
+        await session.deleteSession();
+    });
 });
