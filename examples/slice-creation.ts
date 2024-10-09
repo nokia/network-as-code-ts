6fd70385e80099585c9dc1b5250d58d76171194a
hbirru <henok.birru@nokia.com>
2024-10-09 11:50:12 +0300

    slice creation example corrected

--- examples/slice-creation.ts
+++ examples/slice-creation.ts
@@ -1,97 +1,107 @@
-// Specialized Network functionalities
-
-// Slice examples:
-
 import { NetworkAsCodeClient } from "network-as-code";
 
 const client = new NetworkAsCodeClient("<your-application-key-here>");
 
-// Creation of a slice:
-// We use the country code (MCC) and network code (MNC) to identify the network
-// Different types of slices can be requested using service type and differentiator
-// Area of the slice must also be described in geo-coordinates
-const mySlice = await client.slices.create(
+let mySlice = await client.slices.create(
     { mcc: "236", mnc: "30" },
     { serviceType: "eMBB", differentiator: "123456" },
     "https://notify.me/here",
     {
-        name: "slice-name",
+        name: "slice-1",
         notificationAuthToken: "replace-with-your-auth-token",
         areaOfService: {
             polygon: [
                 {
-                    latitude: 23.0,
-                    longitude: 23.0
+                    latitude: 47.344,
+                    longitude: 104.349,
                 },
                 {
-                    latitude: 23.0,
-                    longitude: 23.0
+                    latitude: 35.344,
+                    longitude: 76.619,
+                },
+                {
+                    latitude: 12.344,
+                    longitude: 142.541,
+                },
+                {
+                    latitude: 19.43,
+                    longitude: 103.53,
                 },
             ],
         },
-        maxDataConnections: 1000,
-        maxDevices: 5,
+        maxDataConnections: 10,
+        maxDevices: 6,
         sliceDownlinkThroughput: {
             guaranteed: 3415,
-            maximum: 1234324
+            maximum: 1234324,
         },
         sliceUplinkThroughput: {
             guaranteed: 3415,
-            maximum: 1234324
+            maximum: 1234324,
         },
         deviceDownlinkThroughput: {
             guaranteed: 3415,
-            maximum: 1234324
+            maximum: 1234324,
         },
         deviceUplinkThroughput: {
             guaranteed: 3415,
-            maximum: 1234324
-        }
+            maximum: 1234324,
+        },
     }
 );
 
-// Get a slice by using an index
-// or remove the index '[0]' to get all slices
-const sliceByIndex = await client.slices.getAll()[0];
-
-// Get a slice using the ID parameter
-const sliceByName = await client.slices.get(mySlice.name as string);
-
-// Modify a slice:
-// Remember to reconfigure all the parameters you wish to modify,
-// The modify() method does not preserve previously configure ones.
-mySlice.modify(
-    {
-        sliceDownlinkThroughput: { guaranteed: 10, maximum: 10 },
-        sliceUplinkThroughput: { guaranteed: 10, maximum: 10 },
-        deviceDownlinkThroughput: { guaranteed: 10, maximum: 10 },
-        deviceUplinkThroughput: { guaranteed: 10, maximum: 10 },
-        maxDataConnections: 12,
-        maxDevices: 3
-    }
-);
+console.log("Slice created", await mySlice);
 
 // We can take advantage of Slice.waitFor() in async functions
 // This allows us to, e.g., wait for a slice to become available
 await mySlice.waitFor("AVAILABLE");
 
+console.log("Slice is now operational");
+
+mySlice = await client.slices.get("slice-1");
+
 // Slices must be activated before devices can be added
 await mySlice.activate();
+
 await mySlice.waitFor("OPERATING");
 
-// Afterwards we can attach or detach devices
-const device = client.devices.get(myDevice)
+console.log("Slice is active and ready for devices");
+
+const device = client.devices.get({
+    ipv4Address: {
+        publicAddress: "1.1.1.2",
+        privateAddress: "1.1.1.2",
+        publicPort: 80,
+    },
+    phoneNumber: "+33670123456",
+});
+
 await mySlice.attach(
-    myDevice,
-    "https://notify.me/here",
-    "replace-with-your-auth-token"
+    device,
+    "c8974e592c2fa383d4a3960714",
+    "https://example.com/notifications",
+    {
+        apps: {
+            os: "97a498e3-fc92-5c94-8986-0333d06e4e47",
+            apps: ["ENTERPRISE"],
+        },
+    }
 );
 
-await mySlice.detach(myDevice);
+console.log("Device attached");
 
-// For deallocating a slice, we first deactivate the slice
+const sleep = (ms: any) => new Promise((r) => setTimeout(r, ms));
+await sleep(10000);
+
+await mySlice.detach(device);
+console.log("Device detached");
+
+// For unallocating a slice, we first deactivate the slice
 await mySlice.deactivate();
 await mySlice.waitFor("AVAILABLE");
 
+console.log("Slice deactivated");
+
 // A deactivated slice can be freely removed
-mySlice.delete()
+await mySlice.delete();
+console.log("Slice deleted");
