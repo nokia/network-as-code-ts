cc77b3ff1f3d6ba50635d97aae3d713b5fd1457d
Abdelaziz Dhaouadi <abdelaziz.dhaouadi@nokia.com>
2025-02-20 15:14:52 +0100

    adding intefration tests for geofencing

--- integration-tests/geofencing.itest.ts
+++ integration-tests/geofencing.itest.ts
@@ -1,4 +1,5 @@
 import { NetworkAsCodeClient } from "../src";
+import { beforeAll, describe, expect} from "@jest/globals";
 import "dotenv/config";
 import { Device } from "../src/models/device";
 import { configureClient } from "./configClient";
@@ -9,27 +10,134 @@ let device: Device;
 beforeAll(() => {
     client = configureClient();
     device = client.devices.get({
-        networkAccessIdentifier: "test-device@testcsp.net",
-        ipv4Address: {
-            publicAddress: "1.1.1.2",
-            privateAddress: "1.1.1.2",
-            publicPort: 80,
-        },
+        phoneNumber:"+3637123456",
     });
 });
 
 describe("Geofencing", () => {
-    it.failing("can subscribe for geofencing events", async () => {
+    it("should subscribe for geofencing event area entered", async () => {
         const subscription = await client.geofencing.subscribe(device, {
             sink: "https://example.com/notif",
             types: ["org.camaraproject.geofencing-subscriptions.v0.area-entered"],
             latitude: 47.48627616952785,
             longitude: 19.07915612501993,
             radius: 2000
-        })
+        });
 
-        expect(subscription.eventSubscriptionId).toBeTruthy()
+        expect(subscription.eventSubscriptionId).toBeTruthy();
 
-        subscription.delete()
-    })
-})
+        subscription.delete();
+    });
+
+    it("should subscribe for geofencing event area left", async () => {
+        const subscription = await client.geofencing.subscribe(device, {
+            sink: "https://example.com/notif",
+            types: ["org.camaraproject.geofencing-subscriptions.v0.area-left"],
+            latitude: 47.48627616952785,
+            longitude: 19.07915612501993,
+            radius: 2000
+        });
+
+        expect(subscription.eventSubscriptionId).toBeTruthy();
+
+        subscription.delete();
+    });
+
+    it("should subscribe for geofencing event with plain credential", async () => {
+        const subscription = await client.geofencing.subscribe(device, {
+            sink: "https://example.com/notif",
+            types: ["org.camaraproject.geofencing-subscriptions.v0.area-left"],
+            latitude: 47.48627616952785,
+            longitude: 19.07915612501993,
+            radius: 2000,
+            sinkCredential: {
+                credentialType:"PLAIN",
+                identifier: "client-id",
+                secret: "client-secret"
+            }
+        });
+
+        expect(subscription.eventSubscriptionId).toBeTruthy();
+    
+        subscription.delete();
+    });
+
+    it("should subscribe for geofencing event with accesstoken credential", async () => {
+        const expirationDate = new Date(Date.now() + 5 * 60 * 60 * 1000);
+        expirationDate.setMilliseconds(0);
+        const subscription = await client.geofencing.subscribe(device, {
+            sink: "https://example.com/notif",
+            types: ["org.camaraproject.geofencing-subscriptions.v0.area-left"],
+            latitude: 47.48627616952785,
+            longitude: 19.07915612501993,
+            radius: 2000,
+            sinkCredential: {
+                credentialType:"ACCESSTOKEN",
+                accessToken: "some-access-token",
+                accessTokenType: "bearer",
+                accessTokenExpiresUtc: expirationDate
+            }
+        });
+
+        expect(subscription.eventSubscriptionId).toBeTruthy();
+    
+        subscription.delete();
+    });
+
+    it("should get an event subscription", async () => {
+        const subscription = await client.geofencing.subscribe(device, {
+            sink: "https://example.com/notif",
+            types: ["org.camaraproject.geofencing-subscriptions.v0.area-entered"],
+            latitude: 47.48627616952785,
+            longitude: 19.07915612501993,
+            radius: 2000
+        });
+
+        expect(subscription.eventSubscriptionId).toBeTruthy();
+
+        const fetchedSubscription = await client.geofencing.get(subscription.eventSubscriptionId as string);
+
+        expect(fetchedSubscription.eventSubscriptionId).toBeTruthy();
+
+        subscription.delete();
+    });
+    
+    it("should get all event subscriptions", async () => {
+        const subscription = await client.geofencing.subscribe(device, {
+            sink: "https://example.com/notif",
+            types: ["org.camaraproject.geofencing-subscriptions.v0.area-entered"],
+            latitude: 47.48627616952785,
+            longitude: 19.07915612501993,
+            radius: 2000
+        });
+
+        expect(subscription.eventSubscriptionId).toBeTruthy();
+
+        const fetchedSubscriptions = await client.geofencing.getAll();
+
+        expect(fetchedSubscriptions.length).toBeGreaterThanOrEqual(0);
+
+        subscription.delete();
+    });
+
+    it("should delete an event subscription", async () => {
+        const subscription = await client.geofencing.subscribe(device, {
+            sink: "https://example.com/notif",
+            types: ["org.camaraproject.geofencing-subscriptions.v0.area-entered"],
+            latitude: 47.48627616952785,
+            longitude: 19.07915612501993,
+            radius: 2000
+        });
+
+        expect(subscription.eventSubscriptionId).toBeTruthy();
+
+        subscription.delete();
+
+        try {
+            await client.geofencing.get(subscription.eventSubscriptionId);
+            expect(true).toBe(false);
+        } catch (error){
+            expect(error).toBeDefined();
+        }
+    });
+});
