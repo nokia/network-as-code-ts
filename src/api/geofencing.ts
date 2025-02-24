3c2c5bef30bf930490795b2fdecaea7911f4f490
Abdelaziz Dhaouadi <abdelaziz.dhaouadi@nokia.com>
2025-02-24 17:00:42 +0100

    adding conversion for sink credentials to json

--- src/api/geofencing.ts
+++ src/api/geofencing.ts
@@ -65,7 +65,7 @@ export class GeofencingAPI {
         };
 
         if (params.sinkCredential) {
-            body.sinkCredential = params.sinkCredential;
+            body.sinkCredential = params.sinkCredential ? Object.fromEntries(Object.entries(params.sinkCredential as {[key:string]: any}).filter(([key, value]) => value !== null && value !== undefined)) : undefined;
         }
 
         if (params.subscriptionExpireTime) {
