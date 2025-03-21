2209ee5b366e9154297f9779c3d39ab5054cc5f1
Sami Lahtinen <sami.lahtinen@nokia.com>
2025-03-21 13:38:47 +0200

    This is how we should do this

--- jest.config.integration.ts
+++ jest.config.integration.ts
@@ -1,7 +1,4 @@
 import config from './jest.config';
 config.testRegex = 'itest\\.ts$';
-testPathIgnorePatterns: [
-    "integration-tests/geofencing.test.ts"
-],
 console.log('Running Integration Test');
 module.exports = config;
--- jest.config.ts
+++ jest.config.ts
@@ -162,7 +162,8 @@ const config: Config = {
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   testPathIgnorePatterns: [
-      "tests/geofencing.test.ts"
+      "tests/geofencing.test.ts",
+      "integration-tests/geofencing.itest.ts"
   ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
