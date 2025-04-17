e5f3e91fe673f33808491f0a882d8f2a23e0383f
Abdelaziz Dhaouadi <abdelaziz.dhaouadi@nokia.com>
2025-04-17 13:17:25 +0200

    Increasing timeout

--- integration-tests/geofencing.itest.ts
+++ integration-tests/geofencing.itest.ts
@@ -39,7 +39,7 @@ describe("Geofencing", () => {
         notification = await fetch(`${notificationUrl}/geofencing-subscriptions/delete/${subscription.eventSubscriptionId}`, { method: 'DELETE' });
 
         subscription.delete();
-    },10 * 1000);
+    },20 * 1000);
 
     it("should subscribe for geofencing event area left", async () => {
         const subscription = await client.geofencing.subscribe(device, {
@@ -61,7 +61,7 @@ describe("Geofencing", () => {
         notification = await fetch(`${notificationUrl}/geofencing-subscriptions/delete/${subscription.eventSubscriptionId}`, { method: 'DELETE' });
 
         subscription.delete();
-    }, 10* 1000);
+    }, 20* 1000);
 
     it("should subscribe for geofencing event with plain credential", async () => {
         const subscription = await client.geofencing.subscribe(device, {
@@ -88,7 +88,7 @@ describe("Geofencing", () => {
         notification = await fetch(`${notificationUrl}/geofencing-subscriptions/delete/${subscription.eventSubscriptionId}`, { method: 'DELETE' });
 
         subscription.delete();
-    }, 10 * 1000);
+    }, 20 * 1000);
 
     it("should subscribe for geofencing event with accesstoken credential", async () => {
         const expirationDate = new Date(Date.now() + 5 * 60 * 60 * 1000);
@@ -118,7 +118,7 @@ describe("Geofencing", () => {
         notification = await fetch(`${notificationUrl}/geofencing-subscriptions/delete/${subscription.eventSubscriptionId}`, { method: 'DELETE' });
 
         subscription.delete();
-    }, 10 * 1000);
+    }, 20 * 1000);
 
     it("should get an event subscription", async () => {
         const subscription = await client.geofencing.subscribe(device, {
